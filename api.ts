90e00a9613f8fe379367bf4b02009f0dbbdb99cc
Ryan <ryanfox1@hotmail.co.uk>
2024-06-17 19:04:55 +0100

    2nd commit

--- api.ts
+++ api.ts
@@ -1,69 +1,83 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const slugMap: Record<string, string> = {
-    "er41": "www.google.com"
-}
-const urlMap: Record<string, string> = {
-    "www.google.com": "er41"
-}
+type Url = {
+  slug: string;
+  url: string;
+};
 
+const urls: Url[] = [
+  {
+    slug: "er41",
+    url: "www.google.com",
+  },
+];
 
 app.get("/t/:slug", (req, res) => {
-    let slug = req.params.slug;
-    let redirectLink = slugMap[slug];
-    if (!redirectLink) {
-        return res.status(404).json({ "message": 'url does not exist'});
-        
-    }
-    return res.redirect(redirectLink)
+  let slug = req.params.slug;
+  let redirectLink = urls.find((e) => e.slug === slug);
+  if (!redirectLink) {
+    return res.status(404).json({ message: "url does not exist" });
+  }
+  return res.redirect(redirectLink.url);
 });
 
 app.get("/links", (req, res) => {
-    return res.json(urlMap);
-})
+  return res.json(urls);
+});
 
 app.delete("/links/:id", (req, res) => {
-  const link = req.params.id;
-  if (!urlMap[link]) {
-    return res.status(400).json({ message: "ID does not exist" });}
-    delete urlMap[link]
-    return res.json(urlMap)
-})
-
+  const slug = req.params.id;
+  const urlToDelete = urls.findIndex((e) => e.slug === slug);
+  if (urlToDelete === -1) {
+    return res.status(400).json({ message: "Url cannot be found" });
+  } else {
+    urls.splice(urlToDelete, 1);
+  }
+  return res.status(200).send();
+});
 
 app.post("/", (req, res) => {
-    const url = req.body.url
-    const slug = urlMap[url]
-    if (slug) {
-        return res.json({ slug })
-    }
-    let uniqueSlug: string = generateSlug()
-    let isSlugUnique = false
-    while (!isSlugUnique) {
-        if (!slugMap[uniqueSlug]) {
-            slugMap[uniqueSlug] = url
-            urlMap[url] = uniqueSlug
-            isSlugUnique = true
-        } else {
-            uniqueSlug = generateSlug()
-        }
-    }
-})
+  const url = req.body.url;
+  const found = urls.find((element) => element.url === url);
+  if (found) {
+    return res.json(found); // { "url": x, "slug": y}
+  }
+
+  const slug = generateSlug();
+  const newUrlSlug = {
+    slug,
+    url,
+  };
+  urls.push(newUrlSlug);
+  return res.json(newUrlSlug);
+});
 
 const generateSlug = () => {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890"
-    let newUrl = ''
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFJHIJKLMNOPQRSTUVWXYZ1234567890";
+
+  while (true) {
+    let slug = "";
+
     for (let i = 0; i < 4; i++) {
-        newUrl += chars[Math.floor(Math.random() * chars.length - 1)]
+      slug += chars[Math.floor(Math.random() * chars.length)];
     }
-    return newUrl
-}
+
+    const slugFound = urls.find((element) => element.slug === slug);
+
+    if (slugFound) {
+      continue;
+    } else {
+      return slug;
+    }
+  }
+};
 
 app.listen(4000, () => {
-    console.log("Listening on PORT: 4000");
+  console.log("Listening on PORT: 4000");
 });
